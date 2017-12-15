76cfc172e8b86ae2c6dcd54dfa1e913e0f4c4ecf
Przemek <puza147@gmail.com>
2017-12-15 10:11:59 +0100

    Task_2

--- ES6_Task_2/components/App.js
+++ ES6_Task_2/components/App.js
@@ -23,24 +23,7 @@ App = React.createClass({
         }.bind(this));
     },
 
-    getGif: function(searchingText, callback) {
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
-                var gif = {
-                url: data.fixed_width_downsampled_url,
-                sourceUrl: data.url
-                };
-            callback(gif);
-            }
-        };
-        xhr.send();
-    },
-
-/*    function getGif(searchingText {
+    function getGif(searchingText) {
         return new Promise(
             function(resolve, reject) {
                 const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
@@ -60,7 +43,7 @@ App = React.createClass({
             };
         xhr.open('GET', url);
         xhr.send();
-    });*/
+    });
 
     render: function() {
 
